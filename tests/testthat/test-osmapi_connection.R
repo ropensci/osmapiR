c2f5babf89fd526f5a1063d7caf535d75c74a172
Joan Maspons <joanmaspons@gmail.com>
2023-07-21 00:47:36 +0200

    Test osmapi_connection functions

---
+++ tests/testthat/test-osmapi_connection.R
@@ -0,0 +1,6 @@
+test_that("osmapi_connection works", {
+  expect_identical(set_osmapi_connection(server = "openstreetmap.org"), "https://api.openstreetmap.org")
+  expect_identical(set_osmapi_connection(server = "testing"), "https://master.apis.dev.openstreetmap.org")
+
+  expect_invisible(logout_osmapi())
+})
