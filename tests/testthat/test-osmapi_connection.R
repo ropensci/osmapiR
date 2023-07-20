88b8e02b57906c6a9e753ae888f2a13df798dad0
Joan Maspons <joanmaspons@gmail.com>
2023-07-21 01:35:38 +0200

    Mock call for test-osmapi_connection

--- tests/testthat/test-osmapi_connection.R
+++ tests/testthat/test-osmapi_connection.R
@@ -2,5 +2,13 @@ test_that("osmapi_connection works", {
   expect_identical(set_osmapi_connection(server = "openstreetmap.org"), "https://api.openstreetmap.org")
   expect_identical(set_osmapi_connection(server = "testing"), "https://master.apis.dev.openstreetmap.org")
 
-  # expect_invisible(logout_osmapi()) # Error in `loadNamespace(x)`: there is no package called 'httpuv'
+  with_mock_dir("mock_osmapi_authenticate", {
+    display_name <- expect_invisible(authenticate_osmapi())
+  })
+
+  expect_identical(display_name, c(display_name = "jmaspons"))
+
+  # with_mock_dir("mock_osmapi_logout", {
+  #   expect_invisible(logout_osmapi()) # Error in `loadNamespace(x)`: there is no package called 'httpuv' (R-CMD-check)
+  # })
 })
