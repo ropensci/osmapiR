8e51ab2f7ff806cc966dd5e69c0d5b865f9a0990
Joan Maspons <joanmaspons@gmail.com>
2023-08-03 13:45:30 +0200

    Improve messages for server side errors by better parsing responses

--- R/osmapi_request.R
+++ R/osmapi_request.R
@@ -3,10 +3,33 @@ osmapi_request <- function(authenticate = FALSE) {
   req <- httr2::req_url_path(req, "api", getOption("osmapir.api_version"))
   req <- httr2::req_retry(req, max_tries = 10L)
   req <- httr2::req_user_agent(req, string = "osmapiR (https://github.com/jmaspons/osmapiR)")
+  req <- httr2::req_error(req, body = error_body)
 
   if (authenticate && !getOption("osmapir.R_CMD_check", FALSE)) {
     req <- oauth_request(req) # nocov
   }
 
   return(req)
+}
+
+
+error_body <- function(resp) {
+  out <- switch(httr2::resp_content_type(resp),
+    "text/plain" = httr2::resp_body_string(resp),
+    "text/html" = parse_html_error(resp),
+    httr2::resp_headers(resp)$status
+  )
+
+  return(out)
+}
+
+parse_html_error <- function(resp) {
+  if (length(resp$body) == 0) {
+    return(httr2::resp_headers(resp)$status)
+  }
+  html <- httr2::resp_body_html(resp)
+  msg <- xml2::xml_children(xml2::xml_find_all(html, ".//div"))
+  out <- xml2::xml_text(msg)
+
+  return(out)
 }
