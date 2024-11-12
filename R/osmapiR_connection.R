171b8bcfec19c3c83ac07e6029eba4df38e0ee19
Joan Maspons <joanmaspons@gmail.com>
2024-11-12 08:40:06 +0100

    Use the new function `httr2::oauth_cache_clear()` from httr2 release 1.0.6

--- R/osmapiR_connection.R
+++ R/osmapiR_connection.R
@@ -158,18 +158,11 @@ authenticate_osmapi <- function() {
 #' @rdname authenticate_osmapiR
 #' @export
 logout_osmapi <- function() {
-
-  if (utils::packageVersion("httr2") >= "1.0.5.9000") {
-    getFromNamespace("oauth_cache_clear", "httr2")(
-      client = oauth_client_osmapi(),
-      cache_disk = getOption("osmapir.cache_authentication"),
-      cache_key = getOption("osmapir.base_api_url")
-    )
-  } else {
-    req <- httr2::request(base_url = get_osmapi_url())
-    req <- oauth_request(req)
-    req$policies$auth_oauth$cache$clear()
-  }
+  httr2::oauth_cache_clear(
+    client = oauth_client_osmapi(),
+    cache_disk = getOption("osmapir.cache_authentication"),
+    cache_key = getOption("osmapir.base_api_url")
+  )
 
   message("Logged out from ", get_osmapi_url())
 
